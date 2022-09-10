9fa8a0110273f96a91707888395c74d4d4d1c6c4
Gabriel Pereira Soares <gabriel.soares14563@gmail.com>
2022-09-10 12:24:17 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  setState = {
+    url: []
+  };
+
+  handleButtonClick(){
+    this.url = '';
+  }
+
+  render(){
+    return (
+      <div className="card">
+        <form action={url}>
+          <label>Type a github username</label><br/>
+          <input type='text'></input>
+          <button onClick={handleButtonClick}>Submit</button>
+        </form>
+        {/* <form action="/action_page.php">
+          <label for="fname">First name:</label><br>
+          <input type="text" id="fname" name="fname" value="John"><br>
+          <label for="lname">Last name:</label><br>
+          <input type="text" id="lname" name="lname" value="Doe"><br><br>
+          <input type="submit" value="Submit">
+        </form>  */}
+      </div>
+    );
+  }
+
 }
+
+// getUserInfo(url){
+//   const info = fetch(url);
+// }
 
 export default App;
